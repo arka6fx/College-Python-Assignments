95b0077f9f2d4d446f3f90ed7f0b69b3738bffba
Arka Garai <arkagarai292@gmail.com>
2024-08-03 19:35:24 +0530

    Update 4prob.py

--- Youtube/CHAPTER 1/4prob.py
+++ Youtube/CHAPTER 1/4prob.py
@@ -1,5 +1,5 @@
 # install an external module [pip install pyttsx3]
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("I love you adrika! infinite times you are my world ")
+engine.say("I love to code")
 engine.runAndWait()
