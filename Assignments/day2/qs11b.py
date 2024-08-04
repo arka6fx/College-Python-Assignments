28fa78177096f90d6ea92c21cb69443307f3e64e
Arka Garai <arkagarai292@gmail.com>
2024-08-04 21:16:42 +0530

    here we go again

--- Assignments/day2/qs11b.py
+++ Assignments/day2/qs11b.py
@@ -1,18 +1,13 @@
 nst=2*5-1
 nsp=0
 n=5
-# for i in range(1,n+1):
-#     for j in range(1,nsp+1):
-#          print("  ", end="")
-#     for k in range(1,nst+1):
-#          print("* ", end="")     
-#     nst-=2
-#     nsp+=1
-#     print()
-    
 for i in range(1,n+1):
-     print("  "*nsp)
-     print("* "*nst)
-     nst-=2
-     nsp+=1
-     print()
+    for j in range(1,nsp+1):
+         print("  ", end="")
+    for k in range(1,nst+1):
+         print("* ", end="")     
+    nst-=2
+    nsp+=1
+    print()
+    
+
