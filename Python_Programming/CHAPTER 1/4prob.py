5b5493030dcd93e4db205a4af81a1ee4df5b1195
Arka Garai <arkagarai292@gmail.com>
2024-08-05 01:52:42 +0530

    changes commited

--- Python_Programming/CHAPTER 1/4prob.py
+++
@@ -1,5 +0,0 @@
-# install an external module [pip install pyttsx3]
-import pyttsx3
-engine = pyttsx3.init()
-engine.say("I love to code")
-engine.runAndWait()
--- Python_Programming/CHAPTER 2/5TypeCasting.py
+++
@@ -1,5 +0,0 @@
-a = "13.4"
-
-b = float(a)
-t = type(a)
-print(t) 
