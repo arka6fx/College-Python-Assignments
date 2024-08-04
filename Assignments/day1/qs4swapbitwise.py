464b238182303206eb2ffe7e39c5872a820a8bf8
Arka Garai <arkagarai292@gmail.com>
2024-08-04 21:11:58 +0530

    changes done

--- Assignments/day1/qs4swapbitwise.py
+++ Assignments/day1/qs4swapbitwise.py
@@ -5,5 +5,5 @@ print("value of y before swapping =",y)
 x=x^y
 y=x^y
 x=x^y
-print("value of x before swapping =",x)
-print("value of y before swapping =",y)
+print("value of x after swapping =",x)
+print("value of y after swapping =",y)
--- Assignments/day2/qs10.py
+++ Assignments/day2/qs10.py
@@ -1,12 +1,12 @@
-principal = float(input("Enter the principal amount: "))
+principle = float(input("Enter the principle amount: "))
 time = float(input("Enter the time period: "))
 
-if principal < 200000:
+if principle < 200000:
     rate = 10
-elif principal >= 200000 and principal <= 1000000:
+elif principle >= 200000 and principle <= 1000000:
     rate = 12
 else:
     rate = 15
 
-simple_interest = (principal * rate * time)/100
+simple_interest = (principle * rate * time)/100
 print("Simple Interest:", simple_interest)
