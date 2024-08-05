81b7cbc8ebc1cf07c100bfc94bbf331237a4694f
Arka Garai <arkagarai292@gmail.com>
2024-08-05 18:49:34 +0530

    f

---
+++ Assignments/day1/01_theory.py
@@ -0,0 +1,12 @@
+# there are total 36 keywords in python
+
+# 3 keywords that start with capital letter
+# True
+# Flase
+# None
+
+# 3/2 = 1.5
+# 3//2 = 1 (floor divison operator)
+# 3.00//2 = 1.0
+# x=5,y=5
+# Two variables pointing to same object
---
+++ Assignments/day2/01_looptheory.py
@@ -0,0 +1,25 @@
+# Conditional Statement
+# if,if-else,if-elif-else
+
+# for and while synatx
+# for var in sequence:  seq==list,tuple,string
+#     statement(s)
+# for i in range(beg,end,step):
+#     statement
+#range(6) => end=6 step=1 beg=0 (by default)
+
+for i in "python":
+    print(i)
+    
+s="python"
+
+for i in range (0,6,1):
+    print(s[i],end="")
+
+
+s="python"
+i=0
+while i<6:
+    print(s[i],end="")
+    i=i+1
+        
