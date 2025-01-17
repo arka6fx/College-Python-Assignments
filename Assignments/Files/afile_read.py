1eef9f255d436d67fcf3c79ea1dde760ad6f458c
Arka Garai <arka292@gmail.com>
2025-01-17 20:30:10 +0530

    c

--- Assignments/Files/afile_read.py
+++
@@ -1,6 +0,0 @@
-file_path = r"C:\Users\arkag\Downloads\Ae Expressions\text expression.txt"
-file = open(file_path, 'r')
-content = file.read()
-print("File contents:")
-print(content)
-file.close()
--- Assignments/oops/class.py
+++
@@ -1,6 +0,0 @@
-class Employee:
-    lang = 'java'
-    salary = 1000000
-    
-arka =  Employee() 
-print(arka.lang,arka.salary)
