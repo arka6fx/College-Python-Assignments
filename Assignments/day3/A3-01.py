17911b3b5e227eb828e52885a8d0fb5307f2a130
Arka Garai <arka292@gmail.com>
2025-01-19 23:57:41 +0530

    c

--- Assignments/day3/A3-01.py
+++ Assignments/day3/A3-01.py
@@ -8,7 +8,7 @@ if number <= 1:
     is_prime = False
 
 else:
-    for i in range(2, round(number**0.5) + 1, 1):
+    for i in range(2, round(number**0.5) + 1):
         if number % i == 0:
             is_prime = False
             break
