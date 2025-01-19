5c4eec4f08c3a8922c5c8ae7c5714ad51922c908
Arka Garai <arka292@gmail.com>
2025-01-19 09:41:36 +0530

    c

--- Assignments/day7/A7_01.py
+++ Assignments/day7/A7_01.py
@@ -9,11 +9,9 @@ def gcd(a, b):
 def lcm(a, b):
     return (a * b) // gcd(a, b) 
 
-# Get input from the user
+
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-
-# Print the results
 print(f"The GCD of {num1} and {num2} is: ",gcd(num1, num2))
 print(f"The LCM of {num1} and {num2} is: ",lcm(num1, num2))
