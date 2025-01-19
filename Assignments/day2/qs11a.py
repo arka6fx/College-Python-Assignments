256a0ffcd864e8bb2990e5042cca4ad8058f8a0b
Arka Garai <arka292@gmail.com>
2025-01-19 23:08:09 +0530

    c

--- Assignments/day2/qs11a.py
+++ Assignments/day2/qs11a.py
@@ -1,10 +1,10 @@
 n = 5  # Number of rows
 
 num = 1
-for i in range(n):
-    for j in range(i):
-        if j==i-1:
-            print(num,end="")
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if j==i:
+            print(num,end=" ")
         else:
             print(num,end=",") 
         num+=1
