6e669d02643ecc5882224b3c3439627619647e1a
Arka Garai <arka292@gmail.com>
2025-01-19 23:06:11 +0530

    c

--- Assignments/day2/qs11a.py
+++ Assignments/day2/qs11a.py
@@ -4,7 +4,7 @@ num = 1
 for i in range(n):
     for j in range(i):
         if j==i-1:
-            print(num,end=" ")
+            print(num,end="")
         else:
             print(num,end=",") 
         num+=1
