62646ba20c77b4ac7609ba8ed53d5cb2a1db04e3
Arka Garai <arka292@gmail.com>
2025-01-20 00:00:04 +0530

    c

--- Assignments/day3/A3-03.py
+++ Assignments/day3/A3-03.py
@@ -1,15 +1,12 @@
 """A3-03.write a program to compute the lcm of two positive integers."""
 num1=int(input("Enter the first number: "))
 num2=int(input("Enter the second number: "))
-
-if num1<=0 or num2<=0:
-    print("LCM is not defined for negative numbers")
-    pass  
-else:    
-    if num1>num2:
-       max_num=num1
-    else:
-       max_num=num2
+  
+    
+if num1>num2:
+    max_num=num1
+else:
+    max_num=num2
     
  
 for i in range(max_num,(num1*num2)+1,max_num):
@@ -19,30 +16,7 @@ for i in range(max_num,(num1*num2)+1,max_num):
         break  
     
         
-# Alternate approach         
-# num1 = int(input("Enter the first number: "))
-# num2 = int(input("Enter the second number: "))
 
-# or_num1 = num1
-# or_num2 = num2
-
-
-# if num1 < num2:
-#     num1, num2 = num2, num1
-
-
-# while num2 != 0:
-#     num1, num2 = num2, num1 % num2 
-#     pass
-#     if num2 == 0:
-#         break
-#     if num1 % 2 != 0:
-#         continue
-
-# hcf = num1
-
-# lcm = (or_num1 * or_num2) / hcf
-# print(f"The LCM is {lcm}.")
 
 
 
